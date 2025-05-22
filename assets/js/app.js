e54bf5be92b4a02f966e62559311697c7eac7bc7
emsroo <emmanueldejesus.rojasramirez@gmail.com>
2025-05-22 13:42:27 -0600

    Se agregan cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,45 @@
+  // endpoint base de github 
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+//selectores mas descriptivos y correccion de query
+const nameEl = document.querySelector('.name');
+const blogEl = document.querySelector('.blog');
+const locationEl = document.querySelector('.location');
+
+
+// Obtiene datos d un usuario de github y los muestraa en el DOM 
+// @Param{string} username  - Nombre de usuario de github
+async function displayUser(username) {
+  nameEl.textContent = 'cargando...';
+  blogEl.textContent = await fetch(`${usersEndpoint}/${username}`);
+   try {
+        const response = await fetch(`${usersEndpoint}/${username}`);
+  if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+  const data= await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+
+  //Mostrar datos en el DOM usando template literals correctamente
+  nameEl.textContent = '${data.name}';
+  blogEl.textContent = '${data.blog}';
+  locationEl.textContent = '${data.location}';
+// }
+
+
+
+
+  } catch (err) {
+        handleError(err);
+      }
+    }
 
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.log('OH NO!', err);
+  // console.log(err);
+  nameEl.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
+// .catch(handleError);
